153d5c1b4027521b4a0f86b13f7e80104541c117
Ahmed Aslam <ahmed.j.aslam@vanderbilt.edu>
2025-06-04 17:00:46 -0500

    yes

--- scripts/models/run_bioemu.sh
+++ scripts/models/run_bioemu.sh
@@ -9,7 +9,7 @@ fi
 PDB_ID=$1
 PDB_DIR="./PDBs/${PDB_ID,,}"
 SEQ_FILE="${PDB_DIR}/${PDB_ID,,}_seq.txt"
-OUTPUT_FILE="${PDB_DIR}/${PDB_ID,,}_bioemu.pdb"
+OUTPUT_FILE="${PDB_DIR}/bioemu_bin/backbone.pdb"
 
 if [ ! -f "$SEQ_FILE" ]; then
     echo "Sequence file not found: $SEQ_FILE"
@@ -20,15 +20,17 @@ SEQUENCE=$(cat "$SEQ_FILE")
 
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
+mkdir -p "$PDB_DIR/bioemu_bin"
+
 python ./models/bioemu/inference.py "$PDB_ID" "$OUTPUT_FILE" "$SEQUENCE" $2
 
-TOPOLOGY_FILE="${PDB_DIR}/${PDB_ID,,}_bioemu.pdb"
 XTC_FILE="${PDB_DIR}/bioemu.xtc"
 
 
-python -m bioemu.sidechain_relax --pdb-path $TOPOLOGY_FILE --xtc-path $XTC_FILE
-echo "[run_bioemu.sh] Saved $PDB_ID to $OUTPUT_FILE"
+python -m bioemu.sidechain_relax --pdb-path $OUTPUT_FILE --xtc-path $XTC_FILE --outpath "$PDB_DIR/bioemu_bin"
+echo "[run_bioemu.sh] Saved backbone of $PDB_ID to $OUTPUT_FILE"
 
-#python ./scripts/helpers/align_to_exp_data.py "$OUTPUT_FILE" "$PDB_DIR/${PDB_ID,,}_final.pdb"
+python ./scripts/helpers/align_to_exp_data.py "$PDB_DIR/bioemu_bin/samples_sidechain_rec.pdb" "$PDB_DIR/${PDB_ID,,}_final.pdb"
+mv "$PDB_DIR/bioemu_bin/samples_sidechain_rec.pdb" "$PDB_DIR/${PDB_ID,,}_bioemu.pdb"
 
 echo "[run_bioemu.sh] Aligned $OUTPUT_FILE to original"
